c414ff8ec96657e4b3ff07203ec697b70c3c070a
SeemranXec <bleh17xec@hotmail.com>
2017-10-06 16:29:58 +0530

    Collection View added

---
+++ App2_Collection/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace App2_Collection
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            id_collection.BackgroundColor = UIColor.Gray;
+            id_collection.RegisterNibForCell(UINib.FromName("cellView", null), cellView.Key);
+            id_collection.DataSource = new Datasrc();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+
+    public class Datasrc : UICollectionViewDataSource
+    {
+
+        public override UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            cellView cell = (cellView)collectionView.DequeueReusableCell(cellView.Key, indexPath);
+            
+            return cell;
+        }
+
+       
+
+        public override nint GetItemsCount(UICollectionView collectionView, nint section)
+        {
+            return 10;
+        }
+    }
+}
---
+++ App2_Collection/cellView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace App2_Collection
+{
+    public partial class cellView : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("cellView");
+        public static readonly UINib Nib;
+
+        static cellView()
+        {
+            Nib = UINib.FromName("cellView", NSBundle.MainBundle);
+        }
+
+        protected cellView(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        internal static cellView Create()
+        {
+            return (cellView)Nib.Instantiate(null, null)[0];
+        }
+    }
+}
